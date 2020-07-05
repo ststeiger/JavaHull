4352329c42312addc0f5c139077487219245ebd9
Me <no.spam@example.com>
2020-07-06 00:11:14 +0200

    Initial commit

---
+++ src/com/company/GeoPoint.java
@@ -0,0 +1,22 @@
+
+package com.company;
+
+
+public class GeoPoint {
+
+    public double Latitude;
+    public double Longitude;
+
+
+    public GeoPoint()
+    {}
+
+    public GeoPoint(double lat, double lng)
+    {
+        this.Latitude = lat;
+        this.Longitude = lng;
+    }
+
+
+
+}
